144d8212e05f1f9aaee09cc54e298bef562fb7e4
Leonardo V Silva <oleovinicius@gmail.com>
2022-07-06 16:56:41 -0300

    Base adction to app Inicial Ios project

---
+++ Carteirinha/Carteirinha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carteirinha
+//
+//  Created by Leonardo on 05/07/22.
+//  Copyright © 2022 Levinsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
